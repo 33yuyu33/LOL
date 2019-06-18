30b26525fa4137d29a915455771b8cb5221a0904
33yuyu33 <2198342152@qq.com>
2019-06-18 20:28:41 +0800

    lol商城

---
+++ heros.js
@@ -0,0 +1,304 @@
+export default [
+    {
+        id:1,
+        name:'玉剑传说 无影',
+        position:'刺客',
+        hero:'泰隆',
+        price:99,
+        sex:'男',
+        // imgUrl:require('./lib/images/yujiantailong.png',改成下面,不然不能打包JS引用的图)片
+        imgUrl:require('./lib/images/yujiantailong.png') ,
+        sales:9999
+    },
+    {
+        id:2,
+        name:'掠星 艾希',
+        position:'ADC',
+        hero:'艾希',
+        price:99,
+        sex:'女',
+        imgUrl:require('./lib/images/luexinghanbing.png'),
+        sales:4624
+    },
+    {
+        id:3,
+        name:'霸天龙王 奥瑞利安索尔',
+        position:'法师',
+        hero:'龙王',
+        price:69,
+        sex:'男',
+        imgUrl:require('./lib/images/batianlongwang.png'),
+        sales:8888
+    },
+    {
+        id:4,
+        name:'西部恶魔 锤石',
+        position:'辅助',
+        hero:'锤石',
+        price:79,
+        sex:'男',
+        imgUrl:require('./lib/images/xibuchuishi.png'),
+        sales:6432
+    },
+    {
+        id:5,
+        name:'未来战士 瑞文',
+        position:'战士',
+        hero:'瑞文',
+        price:59,
+        sex:'女',
+        imgUrl:require('./lib/images/weilaizhanshiruiwen.png'),
+        sales:5979
+    },
+    {
+        id:6,
+        name:'女主播 迦娜',
+        position:'辅助',
+        hero:'迦娜',
+        price:36,
+        sex:'女',
+        imgUrl:require('./lib/images/nvzhubofengnv.png'),
+        sales:4067
+    },
+    {
+        id:7,
+        name:'战场BOSS',
+        position:'法师',
+        hero:'布兰德',
+        price:55,
+        sex:'男',
+        imgUrl:require('./lib/images/zhanchangboss.png'),
+        sales:7942
+    },
+    {
+        id:8,
+        name:'灰烬领主 龙王',
+        position:'法师',
+        hero:'龙王',
+        price:20,
+        sex:'男',
+        imgUrl:require('./lib/images/huijinlingzhu.png'),
+        sales:1953
+    },
+    {
+        id:9,
+        name:'腥红之月 锤石',
+        position:'辅助',
+        hero:'锤石',
+        price:45,
+        sex:'男',
+        imgUrl:require('./lib/images/xinghongchuishi.png'),
+        sales:5932
+    },
+    {
+        id:10,
+        name:'灵魂烈焰',
+        position:'法师',
+        hero:'布兰德',
+        price:10,
+        sex:'男',
+        imgUrl:require('./lib/images/linghunhuonan.png'),
+        sales:1542
+    },
+    {
+        id:11,
+        name:'玉剑传说 风仙子',
+        position:'辅助',
+        hero:'迦娜',
+        price:100,
+        sex:'女',
+        imgUrl:require('./lib/images/yujianfengnv.png'),
+        sales:8502
+    },
+    {
+        id:12,
+        name:'替父从军 花木兰',
+        position:'战士',
+        hero:'瑞文',
+        price:199,
+        sex:'女',
+        imgUrl:require('./lib/images/huamulanruiwen.png'),
+        sales:8032
+    },
+    {
+        id:13,
+        name:'源计划 联合',
+        position:'ADC',
+        hero:'艾希',
+        price:89,
+        sex:'女',
+        imgUrl:require('./lib/images/yuanjihuahanbing.png'),
+        sales:7621
+    },
+    {
+        id:14,
+        name:'符文法师 瑞兹',
+        position:'法师',
+        hero:'瑞兹',
+        price:9.5,
+        sex:'男',
+        imgUrl:require('./lib/images/fuwenfashi.jpg'),
+        sales:7000
+    },
+    {
+        id:15,
+        name:'魔法猫咪 悠米',
+        position:'辅助',
+        hero:'悠米',
+        price:45,
+        sex:'男',
+        imgUrl:require('./lib/images/mofamaomi.jpg'),
+        sales:8888
+    },
+    {
+        id:16,
+        name:'皮城女警 凯特琳',
+        position:'ADC',
+        hero:'凯特琳',
+        price:30,
+        sex:'女',
+        imgUrl:require('./lib/images/pichengnvjing.jpg'),
+        sales:5000
+    },
+    {
+        id:17,
+        name:'圣枪游侠 卢锡安',
+        position:'ADC',
+        hero:'卢锡安',
+        price:45,
+        sex:'男',
+        imgUrl:require('./lib/images/shengqiangyouxia.jpg'),
+        sales:6657
+    },
+    {
+        id:18,
+        name:'暴走萝莉 金克丝',
+        position:'ADC',
+        hero:'金克丝',
+        price:45,
+        sex:'女',
+        imgUrl:require('./lib/images/baozouluoli.jpg'),
+        sales:5870
+    },
+    {
+        id:19,
+        name:'齐天大圣 孙悟空',
+        position:'战士',
+        hero:'孙悟空',
+        price:20,
+        sex:'男',
+        imgUrl:require('./lib/images/qitiandasheng.jpg'),
+        sales:4986
+    },
+    {
+        id:20,
+        name:'放逐之刃 锐雯',
+        position:'战士',
+        hero:'锐雯',
+        price:20,
+        sex:'女',
+        imgUrl:require('./lib/images/fangzhuzhiren.jpg'),
+        sales:9844
+    },
+    {
+        id:21,
+        name:'皎月女神 戴安娜',
+        position:'战士',
+        hero:'戴安娜',
+        price:25,
+        sex:'女',
+        imgUrl:require('./lib/images/jiaoyuenvshen.jpg'),
+        sales:9741
+    },
+    {
+        id:22,
+        name:'德玛西亚之力 盖伦',
+        position:'战士',
+        hero:'盖伦',
+        price:10,
+        sex:'男',
+        imgUrl:require('./lib/images/demaxiyazhili.jpg'),
+        sales:13218
+    },
+    {
+        id:23,
+        name:'德玛西亚皇子 嘉文四世',
+        position:'坦克',
+        hero:'嘉文四世',
+        price:30,
+        sex:'男',
+        imgUrl:require('./lib/images/demaxiyahuangzi.jpg'),
+        sales:4574
+    },
+    {
+        id:24,
+        name:'战争之影 赫卡里姆',
+        position:'坦克',
+        hero:'赫卡里姆',
+        price:35,
+        sex:'男',
+        imgUrl:require('./lib/images/zhanzhengzhiying.jpg'),
+        sales:4894
+    },
+    {
+        id:25,
+        name:'北地之怒 瑟庄妮',
+        position:'坦克',
+        hero:'瑟庄妮',
+        price:35,
+        sex:'女',
+        imgUrl:require('./lib/images/beidizhinu.jpg'),
+        sales:921
+    },
+    {
+        id:26,
+        name:'众星之子 索拉卡',
+        position:'辅助',
+        hero:'索拉卡',
+        price:10,
+        sex:'女',
+        imgUrl:require('./lib/images/zhongxingzhizi.jpg'),
+        sales:9215
+    },
+    {
+        id:27,
+        name:'血港鬼影 派克',
+        position:'辅助',
+        hero:'派克',
+        price:45,
+        sex:'男',
+        imgUrl:require('./lib/images/xuegangguiying.jpg'),
+        sales:912
+    },
+    {
+        id:28,
+        name:'无极剑圣 易',
+        position:'刺客',
+        hero:'易',
+        price:10,
+        sex:'男',
+        imgUrl:require('./lib/images/wujijiansheng.jpg'),
+        sales:15861
+    },
+    {
+        id:29,
+        name:'不祥之刃 卡特琳娜',
+        position:'刺客',
+        hero:'卡特琳娜',
+        price:25,
+        sex:'女',
+        imgUrl:require('./lib/images/katelinna.jpg'),
+        sales:9256
+    },
+    {
+        id:30,
+        name:'时间刺客 艾克',
+        position:'刺客',
+        hero:'艾克',
+        price:45,
+        sex:'男',
+        imgUrl:require('./lib/images/shijiancike.jpg'),
+        sales:921
+    }
+
+]
